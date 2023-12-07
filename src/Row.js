c39cf9e3da5b195e253299273ae2be597bbe8af5
Eric <ericmalah@Erics-MacBook-Pro.local>
2023-12-07 15:24:55 +0100

    new

---
+++ src/Row.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Row({ title }) {
+  return (
+    <div>
+        <h1>{title}</h1>
+        {/* Title */}
+
+        {/* Container -> posters */}
+
+    </div>
+  )
+}
+
+export default Row;
