3a44534bfa0e8decf769229cac70e83aaa9c6d9b
ericel7erek <ericalaa@ymail.com>
2024-04-18 17:41:03 +0200

    Working

--- src/Row.js
+++ src/Row.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
-import YouTube from "react-youtube";
-import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 function Row({ title, fetchUrl, isLargeRow }) {
@@ -12,67 +10,66 @@ function Row({ title, fetchUrl, isLargeRow }) {
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
+      console.log(request.data.results);
       setMovies(request.data.results);
       return request;
     }
     fetchData();
   }, [fetchUrl]);
 
-  const opts = {
-    height: "390",
-    width: "100%",
-    playerVars: {
-      autoplay: 1,
-    },
-  };
-  const handleClick = (movie) => {
-    if (trailerUrl) {
-      setTrailerUrl("");
+const handleClick = (movie) => {
+  if (trailerUrl) {
+    setTrailerUrl("");
+  } else {
+    const movieId = movie.id;
+    let newTrailerUrl;
+
+    if (isLargeRow) {
+      newTrailerUrl = `https://vidsrc.to/embed/tv/${movieId}`;
+    } else if (movie.media_type === "movie") {
+      newTrailerUrl = `https://vidsrc.to/embed/movie/${movieId}`;
+    } else if (movie.media_type === "tv") {
+      newTrailerUrl = `https://vidsrc.to/embed/tv/${movieId}`;
     } else {
-      movieTrailer(
-        movie?.name ||
-          movie?.title ||
-          movie?.original_name ||
-          movie?.original_title
-      )
-        .then((url) => {
-          if (url) {
-            const urlParams = new URLSearchParams(new URL(url).search);
-            setTrailerUrl(urlParams.get("v"));
-          } else {
-            // Handle the case where no trailer is found
-            setTrailerUrl("mHNSMjWqSHE");
-            // You can display a message, show a modal, etc.
-          }
-        })
-        .catch((error) => console.error(error));
+      newTrailerUrl = `https://vidsrc.to/embed/movie/${movieId}`;
     }
-  };
 
-  return (
-    <div className="row">
-      <h1>{title}</h1>
-      <div className="row_posters">
-        {movies.map((movie) => (
-          <img
-            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-            key={movie.id}
-            onClick={() => handleClick(movie)}
-            src={`${base_url}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={`movie.name`}
-            onError={(e) => {
-              e.target.onerror = null;
-              e.target.src = "";
-              e.target.style.display = "none";
-            }}
-          />
-        ))}
-      </div>
-      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    setTrailerUrl(newTrailerUrl);
+  }
+};
+
+return (
+  <div className="row">
+    <h1>{title}</h1>
+    <div className="row_posters">
+      {movies.map((movie) => (
+        <img
+          className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+          key={movie.id}
+          onClick={() => handleClick(movie)}
+          src={`${base_url}${
+            isLargeRow ? movie.poster_path : movie.backdrop_path
+          }`}
+          alt={movie.name}
+          onError={(e) => {
+            e.target.onerror = null;
+            e.target.src = "";
+            e.target.style.display = "none";
+          }}
+        />
+      ))}
     </div>
-  );
-}
-
+    {trailerUrl && (
+      <iframe
+        title="movie-trailer"
+        width="100%"
+        height="390"
+        src={trailerUrl}
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+    )}
+  </div>
+);
+    }
 export default Row;
